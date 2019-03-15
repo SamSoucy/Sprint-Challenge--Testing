090e3fb96aab7ae7315b2b4504b5cdbb7c9160ff
Samsoucy <newf930@outlook.com>
2019-03-15 11:54:32 -0400

    added three tests for GET endpoint

---
+++ api/server.spec.js
@@ -0,0 +1,21 @@
+const server = require("./server");
+const request = require("supertest")
+
+describe("testing the API", () => {
+    describe("return games", () => {
+        it("should return a list of games", async () => {
+            const res = await request(server).get("/games");
+            expect(Array.isArray(res.body)).toBeTruthy();
+        });
+
+        it("should have a status code of 200", async () => {
+            const res = await request(server).get("/games");
+            expect(res.status).toBe(200);
+        });
+
+        it("should return the releaseYear of the game", async () => {
+            const res = await request(server).get("/games");
+            expect(res.body).toBe("releaseYear")
+        });
+    })
+})
